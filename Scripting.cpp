12ebfb85f5bd4246b036f871d20249bf92df30a8
benredmond <bjr0206@gmail.com>
2018-11-03 11:05:34 -0500

    added compile check

--- Scripting.cpp
+++ Scripting.cpp
@@ -1,17 +1,7 @@
-//
-// Created by Ben Redmond on 11/2/18.
-//
-
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <fstream>
-
-int compileFile(std::string file) {
-    std::string cmd = "../compile.sh " + file + ".cpp";
-    const char *cstr = cmd.c_str();
-    return system(cstr);
-}
 
 std::string exec(const char* cmd) {
     FILE* pipe = popen(cmd, "r");
@@ -35,15 +25,11 @@ size_t index (std::string line){
     return index;
 }
 
-int main() {
-    const std::string FILENAME = "main";
-    std::cout << "EXIT CODE: " << compileFile(FILENAME) << std::endl;
-
-    std::string outputCmd = "../compile.sh " + FILENAME + ".cpp";
-    std::string output = exec(outputCmd.c_str());
-    std::cout << "OUTPUT: " << output;
-
-    std::string linterCmd = "cppcheck --enable=all --template=\"{file}:{line}: {severity}: {message}\" --suppress=missingIncludeSystem ../main.cpp 2>&1";
+int main(int argc, char *argv[]) {
+    std::string linterCmd = "cppcheck --enable=all --template=\"{file}:{line}: {severity}: {message}\" "
+            "--suppress=missingIncludeSystem ./cpp/";
+    linterCmd.append(argv[1]);
+    linterCmd.append(".cpp 2>&1");
     std::string linterRes = exec(linterCmd.c_str());
     std::istringstream f(linterRes);
     std::string line;
--- compile.sh
+++ compile.sh
@@ -5,11 +5,11 @@ shift # pull off first arg
 args="$*"
 filename=${input_file%%.cpp}
 
-$CC -o ../$filename.out $CFLAGS ../$input_file
+$CC -o $filename.out $CFLAGS $input_file
 rc=$?
 
 if [[ $rc -eq 0 ]]; then
-   ../$filename.out $args
+   $filename.out $args
    exit $?
 fi
 
